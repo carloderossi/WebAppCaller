615c502f83947abdf79ed97425df50efa487fef7
Carlo De Rossi <cdr.ipad@hotmail.com>
2024-07-24 22:40:59 +0200

    read env from file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-require('dotenv').config();
+//require('dotenv').config();
 
 const App = () => {
    const [cdrCloudIDs, setcdrCloudIDs] = useState([]);
